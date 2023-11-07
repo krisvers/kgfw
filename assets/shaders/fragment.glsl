0170a81c40ee61ceaba9739476a4aab38b2ee0e3
krisvers <105808514+krisvers@users.noreply.github.com>
2023-11-06 22:15:31 -0500

    Update fragment.glsl

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -18,20 +18,6 @@ uniform sampler2D unif_texture_normal;
 out vec4 out_color;
 
 void main() {
-	vec3 normal = normalize(vec3(texture(unif_texture_normal, v_uv)));
-	vec3 light_dir = normalize(unif_light_pos - v_pos);
-	vec3 view_dir = normalize(unif_view_pos - v_pos);
-	float dist = distance(unif_view_pos, v_pos);
-
-	vec3 ambient_color = unif_light_color;
-	
-	vec3 diffuse_color = unif_light_color;
-	float diffusion = unif_diffusion * max(dot(normal, light_dir), 0);
-
-	vec3 H = normalize(light_dir + view_dir);
-	vec3 specular_color = unif_light_color;
-	float speculation = unif_speculation * pow(max(dot(normal, H), 0.0), unif_metalic);
-
 	vec4 tex_color;
 	if (unif_texture_weight == 0) {
 		tex_color = vec4(1, 1, 1, 1);
@@ -42,6 +28,5 @@ void main() {
 		}
 	}
 	
-	vec3 color = (ambient_color * unif_ambience) + (diffuse_color * diffusion) + (specular_color * speculation);
 	out_color = vec4(tex_color * vec4(v_color, 1));
 }
