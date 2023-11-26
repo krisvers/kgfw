7b0df3c9676a4c2f03357eb820ca16519ddfa097
krisvers <nakoidevn@gmail.com>
2023-11-26 15:15:48 -0500

    kobj integration - removed k/, removed test.obj

--- k/kgfw/kgfw_hash.h
+++
@@ -1,9 +0,0 @@
-#ifndef KRISVERS_KGFW_HASH_H
-#define KRISVERS_KGFW_HASH_H
-
-#include "kgfw_defines.h"
-
-KGFW_PUBLIC unsigned long long int kgfw_hash(char * string);
-KGFW_PUBLIC unsigned long long int kgfw_hash_length(char * string, unsigned long long int length);
-
-#endif
